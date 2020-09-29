0af19ddb94ca170a44c18e4a3a8b98e8ac442918
ironboy <thomas@nodehill.com>
2020-09-29 11:44:52 +0200

    toRealFloor method working! + added extensive test in Main.

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -29,4 +29,14 @@ public class LuckySkyScraper {
         return -1; // could not find real floor number
     }
 
+    static public int toFakeFloor(int realFloorGoal){
+        var fakeFloor = 1;
+        for(var realFloor = 1; realFloor < realFloorGoal; realFloor++){
+            do {
+                fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        return fakeFloor;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(LuckySkyScraper.toRealFloor(15));
+        for(var floor = 1; floor <= 220; floor++){
+            System.out.print("floor: " + floor);
+            var fakeFloor = LuckySkyScraper.toFakeFloor(floor);
+            System.out.print("  fake floor: " + fakeFloor);
+            var backToRealFloor = LuckySkyScraper.toRealFloor(fakeFloor);
+            System.out.println("  back to real: " + backToRealFloor);
+        }
     }
 }
