590742e1a829bb59f232ec0458452ee2abb7ad9c
ironboy <thomas@nodehill.com>
2020-09-29 13:51:25 +0200

    Testing memoized version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for(var floor = 1; floor <= 220; floor++){
+
+        // Test to non-memoized version
+        /*for(var floor = 1; floor <= 220; floor++){
             System.out.print("floor: " + floor);
             var fakeFloor = LuckySkyScraper.toFakeFloor(floor);
             System.out.print("  fake floor: " + fakeFloor);
             var backToRealFloor = LuckySkyScraper.toRealFloor(fakeFloor);
             System.out.println("  back to real: " + backToRealFloor);
+        }*/
+
+        // Test memoized version
+        var skyScraper = new LuckySkyScraperMemoized(220);
+        for(var floor = 1; floor <= 220; floor++){
+            System.out.print("floor: " + floor);
+            var fakeFloor = skyScraper.toFakeFloor(floor);
+            System.out.print("  fake floor: " + fakeFloor);
+            var backToRealFloor = skyScraper.toRealFloor(fakeFloor);
+            System.out.println("  back to real: " + backToRealFloor);
         }
+
+
     }
 }
